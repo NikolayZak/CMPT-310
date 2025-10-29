6ea7cbb50055d00da8dba1f2190cf78aed1bace8
Iris <81888973+Shenyuanmeigui12@users.noreply.github.com>
2025-10-29 13:00:09 -0700

    Adding completed Map reading Pipeline
    With test images, it will output binary version of map

---
+++ MapPipeline.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageFilter
+import cv2
+
+# we will be using this file with pipeline to get images from our image processing
+# and read it into a numeric matrix to be read by the machine learning model
+
+# map images are manually blocked out to train the data on
+# first load in the image and set it as a numpy array
+dataMap = Image.open('testMap3.png') # currently a temporary png to test pipeline
+dataMap = dataMap.filter(ImageFilter.SHARPEN) # processing image to read clearer
+edgeMap = dataMap.filter(ImageFilter.FIND_EDGES)
+#dataMap.save("checkMap.png") this was for testing
+#edgeMap.save("edgeMap.png")
+mapArray = np.array(dataMap)
+
+# Get a blank matrix to use to convert the data into binary
+# for path and empty space
+mapSimple = np.empty([mapArray.shape[0],mapArray.shape[1]])
+
+# create a function to input the rgb and return a color
+def color(rgb,aimColor):
+    aimColor = np.array(aimColor)
+    return np.all(np.abs(rgb-aimColor)<10)
+
+# need to figure out the rgb values that are different enough that it won't
+# affect
+for i in range(mapArray.shape[0]): # for loop traverses through to convert map
+    for j in range(mapArray.shape[1]):
+        temp = mapArray[i][j]
+        
+        if (color(temp,[74,173,0])): # check green / empty space
+            mapSimple[i][j] = 1 # this is empty space
+            
+        elif (color(temp,[112,205,255])): # check water 
+            mapSimple[i][j] = 2 # this is empty water space
+            
+        elif (color(temp,[255,179,0])): # check if oragne / tree
+            mapSimple[i][j] = 3
+            
+        elif (color(temp,[255,26,0])): #areas in valide for placing
+            mapSimple[i][j] = 4
+            
+        elif (color(temp,[91,91,91])):
+            mapSimple[i][j] = 0 # check path 
+
+# merging edge map with the full map to get rid of image edge issues
+edgeMap = edgeMap.filter(ImageFilter.BLUR)
+mapSimple[np.sum(edgeMap,axis = 2) != 0] = -1
+mapSimple = mapSimple.astype(int)
+
+# np.savetxt('simpleMap.txt',mapSimple,fmt="%i") testing code
+
+# now to resize the data into 14 * 14 pixel squares
+mapFinal = np.zeros([76,118])
+
+
+for i in range(mapFinal.shape[0]):
+    for j in range(mapFinal.shape[1]):
+        cut = mapSimple[i*7:(i+1)*7,j*7:(j+1)*7]
+        if(np.all(cut == -1)): # getting rid of edge cases where it is all edges
+            mapFinal[i][j] = 4
+            continue
+        values,counts = np.unique(cut[cut != -1],return_counts = True) #gives us count of all values
+        index = np.argmax(counts)# get the index of the value with highest count      
+        mapFinal[i][j] = values[index] #update new map value accordingly
+
+# print(np.unique(mapArray,axis = 0)) for testing
+
+# print(mapFinal)
+# print(mapFinal.shape)
+
+np.savetxt('tester.txt',mapFinal,fmt = '%d')
